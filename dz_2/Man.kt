ccf556479d7d856139e48a7fb3c4dfcfcc365ebb
ArtNikolaev <59572650+ArtNikolaev@users.noreply.github.com>
2020-02-18 00:34:35 +0300

    Update Man.kt

--- dz_2/Man.kt
+++ dz_2/Man.kt
@@ -2,10 +2,9 @@ package dz_2
 
 open class Man(override var name: String, override var age: Int, override var sex: String) : Human {
 
+    var occupation : String ?= null
 
-    var occupation:String?=null
-
-    override fun suy() {
+    override fun say() {
     }
 
     override fun eat() {
