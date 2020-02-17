df2c182703d8fce13b08fa731f069a40d100ba63
ArtNikolaev <59572650+ArtNikolaev@users.noreply.github.com>
2020-02-18 00:35:57 +0300

    Update Mother.kt

--- dz_2/Mother.kt
+++ dz_2/Mother.kt
@@ -7,12 +7,12 @@ open class Mother(name: String, age: Int, sex: String) : Woman(name, age, sex) {
         this.desire = desire
     }
 
-    constructor(name: String,age: Int,sex: String,desire: String, numberOfWorkingDays:Int) : this(name, age, sex, desire){
+    constructor(name: String, age: Int, sex: String, desire: String, numberOfWorkingDays:Int) : this(name, age, sex, desire) {
         this.numberOfWorkingDays = numberOfWorkingDays
     }
 
     override fun say() {
-        println("My name is $name . I am $age years old. My desire is $desire. I work $numberOfWorkingDays days a week")
+        println("My name is $name. I am $age years old. My desire is $desire. I work $numberOfWorkingDays days a week")
         super.say()
     }
 }
