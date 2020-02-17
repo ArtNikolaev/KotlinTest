93ef77c044f62fa9119ae8aef4757d875a13029e
ArtNikolaev <59572650+ArtNikolaev@users.noreply.github.com>
2020-02-18 00:41:33 +0300

    Update Son.kt

--- dz_2/Son.kt
+++ dz_2/Son.kt
@@ -1,7 +1,7 @@
 package dz_2
 
 class Son(name: String, age: Int, sex: String, occupation: String, hasFamily: Boolean) : Father(name, age, sex, occupation, hasFamily) {
-    override fun suy() {
+    override fun say() {
         println("My name is $name. I am $age years old. My occupation is $occupation. I has $hasFamily family.")
     }
 }
