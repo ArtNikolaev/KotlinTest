266f86169a15f825ec0dbb5909a81083ef3f880e
ArtNikolaev <59572650+ArtNikolaev@users.noreply.github.com>
2020-02-17 14:03:34 +0200

    DZ - 2 - HUMAN

---
+++ dz_2/Main.kt
@@ -0,0 +1,27 @@
+package dz_2
+
+class Main
+fun main() {
+    val father = Father("Dima", 50, "Man", "Cook", true)
+    val mother = Mother("Rita", 45, "Woman", "fly into space", 3)
+    val son = Son("Poll", 20, "Man", "Driver", false)
+    val daughter = Daughter("Kate", 22, "Woman", "Family", 5)
+    father.suy()
+    mother.suy()
+    son.suy()
+    daughter.suy()
+
+    val catalog = ArrayList<Human>()
+    catalog.add(father)
+    catalog.add(mother)
+    catalog.add(son)
+    catalog.add(daughter)
+    catalog.sortBy{
+      it.age
+    }
+    catalog.reverse()
+        catalog.forEach {
+        print(it.name + " - " + it.age + ", ")
+    }
+
+}
---
+++ dz_2/Woman.kt
@@ -0,0 +1,15 @@
+package dz_2
+
+open class Woman(override var name: String, override var age: Int, override var sex: String) :Human {
+
+    var desire:String?=null
+
+    override fun suy() {
+    }
+
+    override fun eat() {
+    }
+
+    override fun work() {
+    }
+}
